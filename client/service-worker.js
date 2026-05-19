281f1dd90e1101fe3269d5f415ef778c1e388bed
Orim Dominic <orimdominic@gmail.com>
2026-05-19 22:56:37 +0100

    ft: display list search result on the frontend

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,7 +1,9 @@
 // Register event listener for the 'push' event.
 self.addEventListener("push", function (event) {
-	const payload = event.data.text() ?? `{"title":"Notification","body":"Notification received"}`;
-  const notif = JSON.parse(payload)
+  const payload =
+    event.data.text() ??
+    `{"title":"Notification","body":"Notification received"}`;
+  const notif = JSON.parse(payload);
   // Keep the service worker alive until the notification is created.
   event.waitUntil(
     // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
